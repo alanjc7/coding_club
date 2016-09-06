a8dab897e4283bdfad5cd6c81e94870f65fd61ef
Alan Carrie <alancarrie@Alan-Carries-MacBook.local>
2016-09-06 21:18:35 +0100

    git from terminal

---
+++ lessons/lesson_1/new_ruby_file.rb
@@ -0,0 +1,8 @@
+#this is my first first ruby output.
+puts "Hello, world!"
+puts "another new line put here by puts"
+puts "and more"
+
+print "My name is Alan\n"
+print "forward slash n does the same thing \n"
+print "more printed text"
