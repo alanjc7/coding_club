7874571d3dfb36c62a119a8236bb83d611877998
alanjc7 <alan_c1@hotmail.com>
2016-09-06 21:41:31 +0100

    Delete new_ruby_file.rb

--- lessons/lesson_1/new_ruby_file.rb
+++
@@ -1,8 +0,0 @@
-#this is my first first ruby output.
-puts "Hello, world!"
-puts "another new line put here by puts"
-puts "and more"
-
-print "My name is Alan\n"
-print "forward slash n does the same thing \n"
-print "more printed text"
