13b4f2cae7f0918655cfe3325ea8c19d412b01f3
Alan <alan_c1@hotmail.com>
2016-10-03 19:59:34 +0100

    lesson 5

---
+++ lessons/lesson_5/exercise_5.rb
@@ -0,0 +1,27 @@
+def i_do_nothing()
+end
+
+i_do_nothing()
+
+def is_even?(number)
+	ans = number%2 
+	if ans == 1 
+		puts"no"
+	else 
+		puts"yes"
+	end
+	return
+end
+
+is_3_even = is_even?(3)
+is_4_even = is_even?(4)
+is_5_even = is_even?(5)
+
+puts is_3_even
+
+puts is_4_even
+
+puts is_5_even
+
+
+is_even?(7)
